54a1782118df4d167c97e92ea82147e5668dda19
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-04-28 00:07:44 -0400

    add button to change grid size up to 100x100

--- myjs.js
+++ myjs.js
@@ -1,17 +1,59 @@
-let rows = 16;
-let columns = 16;
+//starting values for 16x16 grid with 800x800 container width,height
+//to change container size, adjust these variables accordingly
+let defaultRows = 16;
+let defaultColumns = defaultRows;
+let defaultSqSize = 50;
+let defaultContainer = 800;
 
-const frame = document.createElement('div');
-frame.className = 'frame';
-    for (let i = 0; i < rows; ++i) {
-        console.log(i);
-        let row = document.createElement('div');
-        for (let j = 0; j < columns; ++j) {
-            let column = document.createElement('div');
-            column.className = 'square';
-            row.appendChild(column);
+createEtch(defaultRows, defaultColumns, defaultSqSize);
+
+//creates the etch-a-sketch
+function createEtch(rows, columns, sqSize) {
+    const frame = document.createElement('div');
+    frame.className = 'frame';
+        for (let i = 0; i < rows; ++i) {
+            console.log(i);
+            let row = document.createElement('div');
+            for (let j = 0; j < columns; ++j) {
+                let column = document.createElement('div');
+                column.className = 'square';
+                column.style.height = sqSize + 'px';
+                column.style.width = sqSize + 'px';
+                row.appendChild(column);
+            }
+            frame.appendChild(row);  
         }
-        frame.appendChild(row);  
-        
+    document.body.appendChild(frame);
+}
+
+//buton to change grid dimension, calls askUser()
+const changeGrid = document.querySelector('#changeGrid');
+changeGrid.addEventListener('click', askUser);
+
+//prompt user for new grid size input and 
+//check if input is between 1 and 100
+function askUser() {
+    let gridNum = prompt('Enter new grid length/width (any number between 1 and 100)');
+    if (0 < gridNum && gridNum < 101) {
+        modifyGrid(gridNum);
+    } else {
+        alert("Number must be between 1 and 100!");
     }
-document.body.appendChild(frame);
+}
+
+//calculate new individual square size 
+//call deleteEtch
+function modifyGrid (gridNum) {
+    let newSize = (defaultContainer / gridNum);
+    deleteEtch(gridNum, newSize);
+}
+
+//remove old grid & 
+//call original createEtch function with new grid size and dimensions
+function deleteEtch(gridNum, newSize) {
+    const oldGrid = document.getElementsByClassName('square');
+    while (oldGrid[0]) {
+        oldGrid[0].parentNode.removeChild(oldGrid[0]);
+    }
+    createEtch (gridNum, gridNum, newSize);
+}
