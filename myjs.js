e0e07bec8d40d7f698de748b9d98aafa46263200
JoshuaCBrown <[200~129564288+JoshuaCBrown@users.noreply.github.com~>
2023-04-27 23:04:50 -0400

    set up div grid with hover shade effect

--- myjs.js
+++ myjs.js
@@ -3,23 +3,15 @@ let columns = 16;
 
 const frame = document.createElement('div');
 frame.className = 'frame';
-    for (let i = 0; i < columns; ++i) {
+    for (let i = 0; i < rows; ++i) {
         console.log(i);
-        let column = document.createElement('div');
-        column.className = 'column';
-        for (let j = 0; j < rows; ++j) {
-            let row = document.createElement('div');
-            row.className = 'row';
-            row.textContent = i + ' - ' + j;
-            column.appendChild(row);
+        let row = document.createElement('div');
+        for (let j = 0; j < columns; ++j) {
+            let column = document.createElement('div');
+            column.className = 'square';
+            row.appendChild(column);
         }
-        frame.appendChild(column);  
+        frame.appendChild(row);  
+        
     }
 document.body.appendChild(frame);
-
-
-
-
-
-
-
